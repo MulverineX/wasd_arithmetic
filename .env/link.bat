1b51a91f1331323e8a04d7e1f9adcd53839fda1c
Mulverine Gaming <mulverin3@gmail.com>
2020-12-17 10:36:41 -0700

    Added symlinking

---
+++ .env/link.bat
@@ -0,0 +1,2 @@
+mklink /J C:\Programs\multimc\instances\1.16.3\.minecraft\saves\NewWorld\datapacks\mountedwasd C:\Users\mulve\Documents\workspace\minecraft\sandstone\mountedwasd\.pack\mountedwasd
+pause
