938d9113352b233396710199a355deaeae6b2805
Mulverine Gaming <mulverin3@gmail.com>
2020-12-18 16:55:30 -0700

    Clean repo

--- sandstone.config.ts
+++
@@ -1,9 +0,0 @@
-import { SandstoneConfig } from 'sandstone/core'
-
-export default {
-  name: 'wasd',
-  description: [ 'A data pack library for detecting WASD input while mounted' ],
-  formatVersion: 6,
-  namespace: 'wasd',
-  saveOptions: { path: './.pack' }
-} as SandstoneConfig
