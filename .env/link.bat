d4c32d39f4766253414fb2324a11728321d2b8b0
Mulverine Gaming <mulverin3@gmail.com>
2020-12-18 13:30:22 -0700

    (Re)move env

--- .env/link.bat
+++
@@ -1,2 +0,0 @@
-mklink /J C:\Programs\multimc\instances\1.16.3\.minecraft\saves\NewWorld\datapacks\mountedwasd C:\Users\mulve\Documents\workspace\minecraft\sandstone\mountedwasd\.pack\mountedwasd
-pause
